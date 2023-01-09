c1d162b96ed7c238c564333ed1baea9bc35bff2f
Faizul <faizul324@gmail.com>
2023-01-09 15:17:51 +0600

    scale all images to proportionate with text

--- src/components/RandomTool.ts
+++ src/components/RandomTool.ts
@@ -10,12 +10,19 @@
 
 import { Container, Sprite, Text, Texture } from "pixijs";
 import { Helper } from "../generic/Helper";
+import { Logger } from "../generic/Logger";
 
 export class RandomTool extends Container {
   private mAddedContents: any[] = [];
+  private mLogger: Logger;
+  private mTextHeight: number;
+  private mContainsText: boolean;
   constructor(params: any[]) {
     super();
+    this.mLogger = new Logger("Random Tool", true);
     this.sortableChildren = true;
+    this.mTextHeight = 0;
+    this.mContainsText = false;
     this.processContents(params);
   }
 
@@ -35,14 +42,17 @@ export class RandomTool extends Container {
       }
     });
     // this.sortChildren();
+    this.updateContentsPosition();
   }
 
   addTextToContainer(message: string): void {
-    let labelTitle: Text = Helper.getLabelWithBasicFont(message);
-    labelTitle.anchor.set(0.5, 0.5);
-    this.addChild(labelTitle);
-    labelTitle.zIndex = 100;
-    this.addContentToList(labelTitle);
+    let text: Text = Helper.getLabelWithBasicFont(message);
+    text.anchor.set(0.5, 0.5);
+    this.addChild(text);
+    text.zIndex = 100;
+    this.addContentToList(text);
+    this.mContainsText = true;
+    this.mTextHeight = text.height;
   }
 
   addImageToContainer(sprite: Sprite): void {
@@ -60,6 +70,57 @@ export class RandomTool extends Container {
 
   addContentToList(content: any): void {
     this.mAddedContents.push(content);
+  }
+
+  updateContentsPosition(): void {
+    this.scaleImages();
+
+    let totalWidth: number = 0;
+    let totalHeight: number = 0;
+
+    this.mAddedContents.forEach((content) => {
+      if (content) {
+        totalWidth += content.width;
+        totalHeight += content.height;
+      }
+    });
+    this.mLogger.Log(
+      "content: w: " +
+        totalWidth +
+        " h: " +
+        totalHeight +
+        " this: w:" +
+        this.width +
+        " h: " +
+        this.height
+    );
+
+    this.width = totalWidth;
+    this.height = totalHeight;
+
+    let currentWidth: number = -totalWidth / 2;
+    for (let i = 0, l = this.mAddedContents.length; i < l; i += 1) {
+      if (this.mAddedContents[i]) {
+        this.mAddedContents[i].position.x =
+          currentWidth + this.mAddedContents[i].width / 2;
+        currentWidth += this.mAddedContents[i].width;
+      }
+    }
+  }
+
+  scaleImages(): void {
+    if (!this.mContainsText || this.mTextHeight == 0) {
+      return;
+    }
+
+    this.mAddedContents.forEach((content) => {
+      if (content && content instanceof Sprite) {
+        // this.mLogger.Log("Before Height: " + content.height);
+        let scale = this.mTextHeight / content.height;
+        content.scale.set(scale, scale);
+        // this.mLogger.Log("After Height: " + content.height);
+      }
+    });
   }
 
   update(delta: number) {
