11157af2dbf1807ac129731154fd9d905e93e442
Faizul <faizul324@gmail.com>
2023-01-09 23:23:56 +0600

    updated description of Particles

--- src/scenes/GameSceneParticles.ts
+++ src/scenes/GameSceneParticles.ts
@@ -1,8 +1,8 @@
 /**
- * Title: Random Tool
- * Description: technical challenge random tool, it is  a tool that will allow mixed text and images in an easy way (for example * displaying text with emoticons or prices with money icon). It should come up every 2 seconds a random text with images in
- * random configuration (image + text + image, image + image + image, image + image + text, text + image + text etc) and a random
- * font size.
+ * Title: Particles - Awesome Fire Tool
+ * Description: open challenge there. Particles - make a demo that shows an awesome fire effect. Please keep
+ * number of images low (max 10 sprites on screen at once). Feel free to use existing libraries how you would
+ *  use them in a real project.
  *
  * Author: Md Faizul Islam
  * Date: 08/01/2023
